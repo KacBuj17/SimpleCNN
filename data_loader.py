0c32c65a813a563debe8e6d26bc0259ac3c55228
KacBuj17 <kbujak150@interia.pl>
2024-12-16 15:25:01 +0100

    Project uploaded

---
+++ data_loader.py
@@ -0,0 +1,17 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+
+def get_data_loaders(batch_size):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+    test_dataset = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
---
+++ test.py
@@ -0,0 +1,24 @@
+import torch
+
+
+def compute_accuracy(correct, total):
+    return 100 * correct / total
+
+
+def test_model(model, test_loader, device):
+    model.to(device)
+    model.eval()
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = compute_accuracy(correct, total)
+    print(f"Test Accuracy: {accuracy:.2f}%")
+    return accuracy
